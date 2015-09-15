a6c2058971138869eb2a24c5a4f62550eb25a075
Julian Acosta <iam@julianacosta.me>
2015-09-15 16:07:42 -0500

    Added announce at bottom of page

--- index.php
+++ index.php
@@ -159,5 +159,22 @@
 			</li>
 		</ul>
 	</aside>
+	<div class="announce">
+		<?php
+			$announce_args = array (
+				'pagename' => 'announce',
+			);
+
+			$announce_query = new WP_Query( $announce_args );
+
+			if ( $announce_query->have_posts() ) {
+				while ( $announce_query->have_posts() ) {
+					$announce_query->the_post();
+					the_content();
+				}
+				wp_reset_postdata();
+			}
+		?>
+	</div>
 </main>
 <?php get_footer() ?>
