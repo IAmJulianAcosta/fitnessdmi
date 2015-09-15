42e53a9dd03c2651fd02f5bad85926bc8f9ccc86
Julian Acosta <iam@julianacosta.me>
2015-09-15 16:17:03 -0500

    Announce under table, font-size 12

--- index.php
+++ index.php
@@ -5,94 +5,113 @@
 	</h1>
 </header>
 <main>
-	<table id="schedule">
-		<thead>
-		<tr>
-			<th class="date"><span class="icon date header-icon"></span></th>
-			<th class="sport"></th>
-			<th class="destination"><span class="icon destination header-icon"></span></th>
-			<th class="location"><span class="icon location header-icon"></span></th>
-			<th class="time"><span class="icon time header-icon"></span></th>
-			<th class="level"><span class="icon level-header"></span></th>
-		</tr>
-		</thead>
-		<tbody>
-		<?php
-			$first_of_month = strtotime( date( '01-m-Y' ) );
-			$last_of_month  = strtotime( date( 't-m-Y 23:59:59' ) );
-			$events_args    = array (
-				'posts_per_page' => 30,
-				'post_type'      => array ( 'event' ),
-				'meta_query'     => array (
-					array (
-						'key'     => 'datetime',
-						'compare' => '>=',
-						'value'   => $first_of_month
+	<div class="main-container">
+		<table id="schedule">
+			<thead>
+			<tr>
+				<th class="date"><span class="icon date header-icon"></span></th>
+				<th class="sport"></th>
+				<th class="destination"><span class="icon destination header-icon"></span></th>
+				<th class="location"><span class="icon location header-icon"></span></th>
+				<th class="time"><span class="icon time header-icon"></span></th>
+				<th class="level"><span class="icon level-header"></span></th>
+			</tr>
+			</thead>
+			<tbody>
+			<?php
+				$first_of_month = strtotime( date( '01-m-Y' ) );
+				$last_of_month  = strtotime( date( 't-m-Y 23:59:59' ) );
+				$events_args    = array (
+					'posts_per_page' => 30,
+					'post_type'      => array ( 'event' ),
+					'meta_query'     => array (
+						array (
+							'key'     => 'datetime',
+							'compare' => '>=',
+							'value'   => $first_of_month
+						),
+						array (
+							'key'     => 'datetime',
+							'compare' => '<=',
+							'value'   => $last_of_month
+						)
 					),
-					array (
-						'key'     => 'datetime',
-						'compare' => '<=',
-						'value'   => $last_of_month
-					)
-				),
-				'orderby'        => 'meta_value_num',
-				'meta_key'       => 'datetime',
-				'order'          => 'ASC'
-			);
-			$events_query   = new WP_Query( $events_args );
-			if ( $events_query->have_posts() ) {
-				while ( $events_query->have_posts() ) {
-					$events_query->the_post();
+					'orderby'        => 'meta_value_num',
+					'meta_key'       => 'datetime',
+					'order'          => 'ASC'
+				);
+				$events_query   = new WP_Query( $events_args );
+				if ( $events_query->have_posts() ) {
+					while ( $events_query->have_posts() ) {
+						$events_query->the_post();
 
-					$datetime_field = get_field( 'datetime' );
-					$day            = utf8_encode( strftime( '%d', $datetime_field ) );
-					$day_of_week    = utf8_encode( strftime( '%a', $datetime_field ) );
-					$hour           = utf8_encode( strftime( '%H', $datetime_field ) );
-					$minute         = utf8_encode( strftime( '%M', $datetime_field ) );
+						$datetime_field = get_field( 'datetime' );
+						$day            = utf8_encode( strftime( '%d', $datetime_field ) );
+						$day_of_week    = utf8_encode( strftime( '%a', $datetime_field ) );
+						$hour           = utf8_encode( strftime( '%H', $datetime_field ) );
+						$minute         = utf8_encode( strftime( '%M', $datetime_field ) );
 
-					$sports = wp_get_post_terms( get_the_ID(), 'event-category' );
-					?>
-					<tr class="event">
-						<td class="date">
-							<time class="event-time">
-								<span class="day-name"><?php echo $day_of_week ?></span>
-								<span class="day"><?php echo $day ?></span>
-							</time>
-						</td>
-						<td class="sport">
-							<?php
-								$has_sport = is_array( $sports ) && sizeof( $sports ) > 0;
-								if ( $has_sport ) {
-									$sport = $sports [0];
-									?><span class="sport icon <?php $sport->slug ?>"></span><?php
-								}
-							?>
-						</td>
-						<td class="destination"><a href="<?php the_field( 'url' ) ?>"><?php the_field(
-									'destination' ) ?></a>
-						</td>
-						<td class="location"><?php the_field( 'location' ) ?></td>
-						<td class="time">
-							<time><?php echo sprintf( '%s:%s', $hour, $minute ) ?></time>
-						</td>
-						<td class="level">
-							<?php
-								$levels    = wp_get_post_terms( get_the_ID(), 'level' );
-								$has_level = is_array( $levels ) && sizeof( $levels ) > 0;
-								if ( $has_level ) {
-									$level = $levels [0];
-									?><span class="<?php echo $level->slug ?> level icon"></span><?php
-								}
-							?>
-						</td>
-					</tr>
-					<?php
+						$sports = wp_get_post_terms( get_the_ID(), 'event-category' );
+						?>
+						<tr class="event">
+							<td class="date">
+								<time class="event-time">
+									<span class="day-name"><?php echo $day_of_week ?></span>
+									<span class="day"><?php echo $day ?></span>
+								</time>
+							</td>
+							<td class="sport">
+								<?php
+									$has_sport = is_array( $sports ) && sizeof( $sports ) > 0;
+									if ( $has_sport ) {
+										$sport = $sports [0];
+										?><span class="sport icon <?php $sport->slug ?>"></span><?php
+									}
+								?>
+							</td>
+							<td class="destination"><a href="<?php the_field( 'url' ) ?>"><?php the_field(
+										'destination' ) ?></a>
+							</td>
+							<td class="location"><?php the_field( 'location' ) ?></td>
+							<td class="time">
+								<time><?php echo sprintf( '%s:%s', $hour, $minute ) ?></time>
+							</td>
+							<td class="level">
+								<?php
+									$levels    = wp_get_post_terms( get_the_ID(), 'level' );
+									$has_level = is_array( $levels ) && sizeof( $levels ) > 0;
+									if ( $has_level ) {
+										$level = $levels [0];
+										?><span class="<?php echo $level->slug ?> level icon"></span><?php
+									}
+								?>
+							</td>
+						</tr>
+						<?php
+					}
 				}
-			}
-		?>
+			?>
 
-		</tbody>
-	</table>
+			</tbody>
+		</table>
+		<div class="announce">
+			<?php
+				$announce_args = array (
+					'pagename' => 'announce',
+				);
+
+				$announce_query = new WP_Query( $announce_args );
+
+				if ( $announce_query->have_posts() ) {
+					while ( $announce_query->have_posts() ) {
+						$announce_query->the_post();
+						the_content();
+					}
+					wp_reset_postdata();
+				}
+			?>
+		</div>
+	</div>
 	<aside class="legend">
 		<ul class="sports-legend">
 			<?php
@@ -159,22 +178,5 @@
 			</li>
 		</ul>
 	</aside>
-	<div class="announce">
-		<?php
-			$announce_args = array (
-				'pagename' => 'announce',
-			);
-
-			$announce_query = new WP_Query( $announce_args );
-
-			if ( $announce_query->have_posts() ) {
-				while ( $announce_query->have_posts() ) {
-					$announce_query->the_post();
-					the_content();
-				}
-				wp_reset_postdata();
-			}
-		?>
-	</div>
 </main>
 <?php get_footer() ?>
