f987ccb82f3bc13853715d7936f96337d81fdfae
Julian Acosta <iam@julianacosta.me>
2015-10-02 18:08:46 -0500

    Github ribbon

--- header.php
+++ header.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" >
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<title><?php wp_title(); ?></title>
 	<link href='https://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<?php
-		wp_head ();
+		wp_head();
 	?>
 </head>
 <body <?php body_class(); ?>>
+<a href="https://github.com/IAmJulianAcosta/fitnessdmi"><img
+		class="github-ribbon"
+		src="https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67"
+		alt="Fork me on GitHub"
+		data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"></a>
