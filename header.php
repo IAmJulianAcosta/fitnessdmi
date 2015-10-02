5d061190d26d84e95a1a92cf7fc5fb6894352330
Julian Acosta <iam@julianacosta.me>
2015-10-02 17:31:33 -0500

    Fixed header.php to comply with wp standards

--- header.php
+++ header.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>
 <head>
-	<meta charset="UTF-8">
-	<title></title>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" >
+	<title><?php wp_title(); ?></title>
 	<link href='https://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<?php
 		wp_head ();
 	?>
